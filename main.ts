bdedb2d2ca11200af0dc37f4ba70bd4bf41e5fd3
ArielChaves <139404966+ArielChaves@users.noreply.github.com>
2023-12-21 18:43:42 -0300

    Delete main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-function multiplication (number1: number, number2: number): number {
-    return number1 * number2;
-}
-        function saudacao(nome: string): string {
-    return `Olá ${nome}`
-}
